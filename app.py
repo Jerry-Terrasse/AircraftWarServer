56823bfc192b5c77b7e47e3644d4fa93afb7180c
Terrasse <terrasse@qq.com>
2023-06-01 12:23:24 +0800

    add records

--- app.py
+++ app.py
@@ -31,6 +31,13 @@ class Fight:
         else:
             raise Exception("User not in fight")
 
+class Record:
+    def __init__(self, name: User, score: int, record_id: int, date: str):
+        self.name = name
+        self.score = score
+        self.record_id = record_id
+        self.date = date
+
 users = [
     User('admin', 'admin'),
     User('user1', 'pass1'),
@@ -40,6 +47,8 @@ users = [
 waiting_list = []
 fighting_list: list[Fight] = []
 rank_list = []
+
+records = []
 
 app = Flask(__name__)
 
@@ -111,8 +120,13 @@ def finish():
             print("{} finish".format(user.username))
             if user == fight.user1:
                 fight.finished1 = True
+                score = fight.score1
             else:
                 fight.finished2 = True
+                score = fight.score2
+            
+            records.append(Record(user.username, score, len(records), str(int(time.time()*1000))))
+            
             if fight.finished1 and fight.finished2:
                 fighting_list.remove(fight)
                 print("{} vs {} finished".format(fight.user1.username, fight.user2.username))
@@ -120,6 +134,16 @@ def finish():
     else:
         return {"status": "failed", "reason": "not in fighting"}
     return {"status": "success"}
+
+@app.get("/get_records")
+def get_records():
+    username = request.form['username']
+    password = request.form['password']
+    
+    user = User(username, password)
+    if user not in users:
+        return {"status": "failed", "reason": "wrong username or password"}
+    return {"status": "success", "records": records}
 
 def clear_fight():
     global fighting_list
